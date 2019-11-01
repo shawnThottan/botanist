2558b38e23694cfdcbf50c2b38591ce1d2dbf0bf
Shawn S. Thottan <shawn.thottan@freshtohome.com>
2019-11-02 02:27:13 +0530

    removed hello world

--- app.py
+++ app.py
@@ -9,11 +9,6 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
 @app.route('/analyse', methods=["GET","POST"])
 def predict_plant_disease():
     try:
