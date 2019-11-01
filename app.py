1b4871f4bc130701894e1d856fe6adba3a4986c1
Shawn S. Thottan <shawn.thottan@freshtohome.com>
2019-11-02 04:57:28 +0530

    changed the model

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 from keras.preprocessing.image import img_to_array
+from keras import backend as K
 from flask import Flask
 from flask import request
 from flask_cors import CORS
@@ -24,35 +25,23 @@ def predict_plant_disease():
             if request.files:
                 image_file = request.files["plant_image"]
                 image_data = base64.b64encode(image_file.read())
-                image_array = 0
                 try:
                     image = Image.open(io.BytesIO(base64.b64decode(image_data)))
                     if image is not None :
-                        image = image.resize(tuple((224, 224)), Image.ANTIALIAS)
+                        image = image.resize(tuple((256, 256)), Image.ANTIALIAS)
                         image_array = np.expand_dims(img_to_array(image), axis=0)
                     else :
                         raise Exception('Error loading image file')
                 except Exception as e:
                     return None, str(e)
-                model_file = f"cnn_model.pkl"
-                
-                interpreter = tf.lite.Interpreter(model_path="optimized_graph.tflite")
-                interpreter.allocate_tensors()
-                input_details = interpreter.get_input_details()
-                output_details = interpreter.get_output_details()
-                
-                interpreter.set_tensor(input_details[0]['index'], image_array)
-
-                interpreter.invoke()
-
-                output_data = interpreter.get_tensor(output_details[0]['index'])
-                best_match_index = np.argmax(output_data)
-
-                with open('retrained_labels.txt', 'r') as f:
-                    matrix = [line.strip() for line in f]
+                K.clear_session()
+                model_file = f"./cnn_model.pkl"
+                saved_classifier_model = pickle.load(open(model_file,'rb'))
+                prediction = saved_classifier_model.predict(image_array) 
+                label_binarizer = pickle.load(open(f"./label_transform.pkl",'rb'))
                 return_data = {
                     "error" : "0",
-                    "data" : matrix[best_match_index]
+                    "data" : f"{label_binarizer.inverse_transform(prediction)[0]}"
                 }
             else :
                 return_data = {
